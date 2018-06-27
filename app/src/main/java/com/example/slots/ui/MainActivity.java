0727c31b8dad332de5c37eb86673f0b7ff7220ea
den <stoplus@gmail.com>
2018-06-27 13:59:26 +0300

    added an additional font and finished the design for the phone, tablet 7 'and tablet 10'

--- app/src/main/java/com/example/slots/ui/MainActivity.java
+++ app/src/main/java/com/example/slots/ui/MainActivity.java
@@ -33,11 +33,28 @@ UserDao userDao;
         MyApp.app().appComponent().inject(this);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        final TextView jackpot = findViewById(R.id.textView_jackpot);
         final TextView firstEditText = findViewById(R.id.textViewCombinations);
-        firstEditText.setTypeface(Typeface.createFromAsset(
-                getAssets(), "Sextan-Bold-FFP.ttf"));
+        final TextView textView_field_lines_descript = findViewById(R.id.textView_field_lines_descript);
+        final TextView coins_descript = findViewById(R.id.coins_descript);
+        final TextView field_bet_descript = findViewById(R.id.field_bet_descript);
+
+        firstEditText.setTypeface(Typeface.createFromAsset(getAssets(), "Sextan-Bold-FFP.ttf"));
+        textView_field_lines_descript.setTypeface(Typeface.createFromAsset(getAssets(), "Sextan-Bold-FFP.ttf"));
+        coins_descript.setTypeface(Typeface.createFromAsset(getAssets(), "Sextan-Bold-FFP.ttf"));
+        field_bet_descript.setTypeface(Typeface.createFromAsset(getAssets(), "Sextan-Bold-FFP.ttf"));
+        jackpot.setTypeface(Typeface.createFromAsset(getAssets(), "Bodonio.ttf"));
     }
 
     public void btnSpin(View view) {
+    }
+
+    public void button_small_minus(View view) {
+    }
+
+    public void button_small_plus(View view) {
+    }
+
+    public void button_blue_small(View view) {
     }
 }
